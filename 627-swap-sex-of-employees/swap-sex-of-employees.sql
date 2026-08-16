bd3240b821043959362a0e8e44dc1c6021ff073b
Harshgupta90211 <guptaharsh90211@gmail.com>
2026-08-16 23:22:20 +0530

    Time: 244 ms (84.45%) | Memory: 0B (100.00%) - LeetSync

--- 627-swap-sex-of-employees/swap-sex-of-employees.sql
+++ 627-swap-sex-of-employees/swap-sex-of-employees.sql
@@ -1,6 +1,1 @@
-# Write your MySQL query statement below
-UPDATE salary SET sex =
-CASE sex
-    WHEN 'm' THEN 'f'
-    ELSE 'm'
-END;
+ update salary set sex = CHAR(ASCII('f') ^ ASCII('m') ^ ASCII(sex));
