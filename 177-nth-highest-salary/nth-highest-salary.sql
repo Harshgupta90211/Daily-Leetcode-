2b53234990fb8a77e205e2815b78c3c386d3d4dc
Harshgupta90211 <guptaharsh90211@gmail.com>
2026-09-24 22:19:56 +0530

    Time: 487 ms (58.84%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,11 +1,11 @@
- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N = N-1;
-  RETURN (
-      SELECT DISTINCT(salary) from Employee order by salary DESC
-      LIMIT 1 OFFSET N
-      
-  );
-END
+    SET N = N - 1;
 
-#pls upvote if you find solution easy to undestand....!! Thanks..!!!
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+    );
+END
